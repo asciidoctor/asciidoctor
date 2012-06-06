518dcf08e9c49769718695d52de7ce45ec726b88
Ryan Waldron <rew@erebor.com>
2012-06-06 17:48:09 -0500

    Properly instantiate document; get one test to pass

--- test/asciidoc_test.rb
+++ test/asciidoc_test.rb
@@ -3,11 +3,14 @@ require 'test_helper'
 class ProjectTest < Test::Unit::TestCase
   # setup for test
   def setup
-    @doc = Asciidoc::Document.new(sample_doc_path(:asciidoc_index))
+    @doc = Asciidoc::Document.new(File.readlines(sample_doc_path(:asciidoc_index)))
   end
 
-  def test_title
-    assert_equal "Documentation", @doc.title
+  def test_root_name
+    assert_equal "AsciiDoc Home Page", @doc.root.name
   end
 
+  def test_is_section_heading
+    assert @doc.is_section_heading?("AsciiDoc Home Page", "==================")
+  end
 end
