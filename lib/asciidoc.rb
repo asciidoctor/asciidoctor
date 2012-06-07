82d8239517a833e9c14e9a0ef3de6d8bb3171db1
Ryan Waldron <rew@erebor.com>
2012-06-06 19:09:22 -0500

    Add another regexp

--- lib/asciidoc.rb
+++ lib/asciidoc.rb
@@ -31,65 +31,68 @@ require 'erb'
 module Asciidoc
   REGEXP = {
     # [[Foo]]  (also allows, say, [[[]] or [[[Foo[f]], but I don't think it is supposed to (TODO))
-    :anchor   => /^\[(\[.+\])\]\s*$/,
+    :anchor           => /^\[(\[.+\])\]\s*$/,
+
+    # Foowhatevs [[Bar]]
+    :anchor_embedded  => /^(.*)\[\[([^\]]+)\]\]\s*$/,
 
     # [[[Foo]]]  (does not suffer quite the same malady as :anchor, but almost. Allows [ but not ] in internal capture
-    :biblio   => /\[\[\[([^\]]+)\]\]\]/,
+    :biblio           => /\[\[\[([^\]]+)\]\]\]/,
 
     # [caption="Foo"]
-    :caption  => /^\[caption=\"([^\"]+)\"\]/,
+    :caption          => /^\[caption=\"([^\"]+)\"\]/,
 
     # <1> Foo
-    :colist   => /^(\<\d+\>)\s*(.*)/,
+    :colist           => /^(\<\d+\>)\s*(.*)/,
 
     # // (and then whatever)
-    :comment  => /^\/\/\s/,
+    :comment          => /^\/\/\s/,
 
     # +   (note that Asciidoc appears to allow continuations using + at the end of the previous line and indenting
     #      the following line (as in :lit_par))
-    :continue => /^\+\s*$/,
+    :continue         => /^\+\s*$/,
 
     # foo::  ||  foo;;
-    :dlist    => /^(\s*)(\S.*)(::|;;)\s*$/,
+    :dlist            => /^(\s*)(\S.*)(::|;;)\s*$/,
 
     # ====
-    :example  => /^={4,}\s*$/,
+    :example          => /^={4,}\s*$/,
 
     # ======  || ------ || ~~~~~~ || ^^^^^^ || ++++++
-    :line     => /^([=\-~^\+])+\s*$/,
+    :line             => /^([=\-~^\+])+\s*$/,
 
     # ----
-    :listing  => /^\-{4,}\s*$/,
+    :listing          => /^\-{4,}\s*$/,
 
     # ....
-    :lit_blk  => /^\.{4,}\s*$/,
+    :lit_blk          => /^\.{4,}\s*$/,
 
     # <TAB>Foo  or one-or-more-spaces-or-tabs then whatever
-    :lit_par  => /^([ \t]+.*)$/,
+    :lit_par          => /^([ \t]+.*)$/,
 
     # "Wooble"  ||  Wooble
-    :name     => /^(["A-Za-z].*)\s*$/,  # I believe this fails to require " chars to be paired (TODO)
+    :name             => /^(["A-Za-z].*)\s*$/,  # I believe this fails to require " chars to be paired (TODO)
 
     # [NOTE]
-    :note     => /^\[NOTE\]\s*$/,
+    :note             => /^\[NOTE\]\s*$/,
 
     # --
-    :oblock   => /^\-\-\s*$/,
+    :oblock           => /^\-\-\s*$/,
 
     # 1.Foo  ||  1. Foo  ||  . Foo
-    :olist    => /^\s*(\d+\.|\. )(.*)$/,
+    :olist            => /^\s*(\d+\.|\. )(.*)$/,
 
     # ____
-    :quote    => /^_{4,}\s*$/,
+    :quote            => /^_{4,}\s*$/,
 
     # .Foo   but not  . Foo or ..Foo
-    :title    => /^\.([^\s\.].*)\s*$/,
+    :title            => /^\.([^\s\.].*)\s*$/,
 
     # * Foo  ||  - Foo
-    :ulist    => /^\s*([\*\-])\s+(.*)$/,
+    :ulist            => /^\s*([\*\-])\s+(.*)$/,
 
     # [verse]
-    :verse    => /^\[verse\]\s*$/
+    :verse            => /^\[verse\]\s*$/
   }
   /(^|[^\\])\{(\w[\w\-]+\w)\}/
 
