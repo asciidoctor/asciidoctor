5530d924e7461dd6592a76e79906b95ce0ecb05e
Ryan Waldron <rew@erebor.com>
2012-06-09 13:42:39 -0500

    Take out silly extra para render from Section template; also avoid silly quote escaping by switching to single quotes

--- lib/asciidoc/render_templates.rb
+++ lib/asciidoc/render_templates.rb
@@ -25,14 +25,14 @@ class DocumentTemplate < BaseTemplate
   def template
     @template ||= ::ERB.new <<-EOF
       <div class='man-page'>
-        <div id="header">
+        <div id='header'>
           <% if document %>
             <% if document.header %>
               <h2><%= document.header.name %></h2>
-              <div class="sectionbody"><%= document.header.content %></div>
+              <div class='sectionbody'><%= document.header.content %></div>
             <% elsif document.preamble %>
-              <div class="preamble">
-                <div class="sectionbody">
+              <div class=preamble'>
+                <div class='sectionbody'>
                   <%= document.preamble.content %>
                 </div>
               </div>
@@ -50,21 +50,18 @@ class SectionTemplate < BaseTemplate
   def template
     @template ||= ERB.new <<-EOF
       <% if !anchor.nil? %>
-        <a name="<%= anchor %>">
+        <a name='<%= anchor %>'>
       <% end %>
-      <div class="sect<%= level %>">
-        <h<%= level + 1 %> id="<%= section_id %>"><%= name %></h<%= level + 1 %>>
+      <div class='sect<%= level %>'>
+        <h<%= level + 1 %> id='<%= section_id %>'><%= name %></h<%= level + 1 %>>
         <% if !anchor.nil? %>
           </a>
         <% end %>
         <% if level == 1 %>
-          <div class="sectionbody"><%= content %></div>
+          <div class='sectionbody'><%= content %></div>
         <% else %>
           <%= content %>
         <% end %>
-      </div>
-      <div class="paragraph">
-        <p><%= content %></p>
       </div>
     EOF
   end
@@ -73,7 +70,7 @@ end
 class SectionAnchorTemplate < BaseTemplate
   def template
     @template ||= ERB.new <<-EOF
-      <a name="<%= content %>"></a>
+      <a name='<%= content %>'></a>
     EOF
   end
 end
@@ -81,8 +78,8 @@ end
 class SectionLiteralTemplate < BaseTemplate
   def template
     @template ||= ERB.new <<-EOF
-      <div class="literalblock">
-        <div class="content">
+      <div class='literalblock'>
+        <div class='content'>
           <pre><tt><%= content %></tt></pre>
         </div>
       </div>
@@ -93,7 +90,7 @@ end
 class SectionParagraphTemplate < BaseTemplate
   def template
     @template ||= ERB.new <<-EOF
-      <div class="paragraph">
+      <div class='paragraph'>
         <p><%= content %></p>
       </div>
     EOF
@@ -103,10 +100,10 @@ end
 class SectionDlistTemplate < BaseTemplate
   def template
     @template ||= ERB.new <<-EOF
-      <div class="dlist">
+      <div class='dlist'>
         <dl>
           <% content.each do |dt, dd| %>
-            <dt class="hdlist1"><%= dt %></dt>
+            <dt class='hdlist1'><%= dt %></dt>
             <% unless dd.nil? || dd.empty? %>
               <dd><%= dd %></dd>
             <% end %>
@@ -120,7 +117,7 @@ end
 class SectionUlistTemplate < BaseTemplate
   def template
     @template ||= ERB.new <<-EOF
-      <div class="ulist">
+      <div class='ulist'>
         <ul>
           <% content.each do |li| %>
             <li><p><%= li %></p></li>
@@ -133,7 +130,7 @@ end
 
 =begin
 ../gitscm-next/templates/section_colist.html.erb
-<div class="colist arabic">
+<div class='colist arabic'>
   <ol>
     <% content.each do |li| %>
       <li><p><%= li %></p></li>
@@ -141,52 +138,52 @@ end
   </ol>
 </div>
 ../gitscm-next/templates/section_example.html.erb
-<div class="exampleblock">
-  <div class="content">
-    <div class="literalblock">
-      <div class="content">
+<div class='exampleblock'>
+  <div class='content'>
+    <div class='literalblock'>
+      <div class='content'>
         <pre><tt><%= content %></tt></pre>
       </div>
     </div>
   </div>
 </div>
 ../gitscm-next/templates/section_listing.html.erb
-<div class="listingblock">
-  <div class="content">
+<div class='listingblock'>
+  <div class='content'>
     <pre><tt><%= content %></tt></pre>
   </div>
 </div>
 ../gitscm-next/templates/section_note.html.erb
-<div class="admonitionblock">
+<div class='admonitionblock'>
   <table>
     <tr>
-      <td class="icon"><div class="title">Note</div></td>
-      <td class="content"><%= content %></td>
+      <td class='icon'><div class='title'>Note</div></td>
+      <td class='content'><%= content %></td>
     </tr>
   </table>
 </div>
 ../gitscm-next/templates/section_oblock.html.erb
-<div class="openblock">
-  <div class="content">
+<div class='openblock'>
+  <div class='content'>
     <%= content %>
   </div>
 </div>
 ../gitscm-next/templates/section_olist.html.erb
-<div class="olist arabic">
-  <ol class="arabic">
+<div class='olist arabic'>
+  <ol class='arabic'>
     <% content.each do |li| %>
       <li><p><%= li %></p></li>
     <% end %>
   </ol>
 </div>
 ../gitscm-next/templates/section_quote.html.erb
-<div class="quoteblock">
-  <div class="content">
+<div class='quoteblock'>
+  <div class='content'>
     <%= content %>
   </div>
 </div>
 ../gitscm-next/templates/section_verse.html.erb
-<div class="verseblock">
-  <pre class="content"><%= content %></pre>
+<div class='verseblock'>
+  <pre class='content'><%= content %></pre>
 </div>
 =end
