2561819957c32cc4cd01a8402e250aa37cb3c818
Ryan Waldron <rew@erebor.com>
2012-06-21 21:57:57 -0500

    Expand and clean up the recursive debug dump empire

--- lib/asciidoctor/debug.rb
+++ lib/asciidoctor/debug.rb
@@ -2,5 +2,12 @@ module Asciidoctor
   def self.debug(*args)
     puts *args unless ENV['SUPPRESS_DEBUG'] == 'true'
   end
+
+  def self.puts_indented(level, *args)
+    thing = " "*level*2
+    args.each do |arg|
+      puts "#{thing}#{arg}"
+    end
+  end
 end
 
