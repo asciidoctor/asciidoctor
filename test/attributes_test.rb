a9beb7b5c2a6bd1b0261ccf8b7f27ba5a8ef00ab
Ryan Waldron <rwaldron@github.com>
2012-07-19 23:12:40 -0500

    Move substition tests into attribute_test.rb

--- test/attributes_test.rb
+++ test/attributes_test.rb
@@ -2,14 +2,55 @@ require 'test_helper'
 
 context "Attributes" do
   test "render properly with simple names" do
-    output = render_string(":frog: Tanglefoot\nYo, {frog}!")
-    results = Nokogiri::HTML::DocumentFragment.parse(output).xpath(".//p")
-    assert_equal 'Yo, Tanglefoot!', results[0].content
+    html = render_string(":frog: Tanglefoot\nYo, {frog}!")
+    result = Nokogiri::HTML(html)
+    assert_equal 'Yo, Tanglefoot!', result.css("p").first.content.strip
   end
 
   test "convert multi-word names and render" do
-    output = render_string("Main Header\n===========\n:My frog: Tanglefoot\nYo, {myfrog}!")
-    results = Nokogiri::HTML::DocumentFragment.parse(output).xpath(".//p")
-    assert_equal 'Yo, Tanglefoot!', results[0].content
+    html = render_string("Main Header\n===========\n:My frog: Tanglefoot\nYo, {myfrog}!")
+    result = Nokogiri::HTML(html)
+    assert_equal 'Yo, Tanglefoot!', result.css("p").first.content.strip
   end
+
+  test "ignores lines with bad attributes" do
+    output = render_string("This is\nblah blah {foobarbaz}\nall there is.")
+    assert_equal 'Yo, Tanglefoot!', output
+  end
+
+  # See above - AsciiDoc says we're supposed to delete lines with bad
+  # attribute refs in them. AsciiDoc is strange.
+  #
+  # test "Unknowns" do
+  #   html = render_string("Look, a {gobbledygook}")
+  #   result = Nokogiri::HTML(html)
+  #   assert_equal("Look, a {gobbledygook}", result.css("p").first.content.strip)
+  # end
+
+  test "deletes an existing attribute" do
+    pending "Not done yet"
+  end
+
+  test "doesn't choke when deleting a non-existing attribute" do
+    pending "Not done yet"
+  end
+
+  test "doesn't disturb escaped attribute-looking things" do
+    pending "Not done yet"
+  end
+
+  test "doesn't subsitute attributes inside code blocks" do
+    pending "whut?"
+  end
+
+  test "doesn't subsitute attributes inside literal blocks" do
+    pending "whut?"
+  end
+
+  test "Intrinsics" do
+    html = render_string("Look, a {caret}")
+    result = Nokogiri::HTML(html)
+    assert_equal("Look, a ^", result.css("p").first.content.strip)
+  end
+
 end
