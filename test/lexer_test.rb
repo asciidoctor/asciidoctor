2ad98dbb52fb03b0005fe6cdd2c502e6d07d6ea7
Ryan Waldron <rwaldron@github.com>
2012-07-31 18:24:57 -0500

    Don't use nasty .send crap now.

--- test/lexer_test.rb
+++ test/lexer_test.rb
@@ -6,7 +6,7 @@ class LexerTest < Test::Unit::TestCase
   end
 
   def test_is_section_heading
-    assert Asciidoctor::Lexer.send(:is_section_heading?, "AsciiDoc Home Page", "==================")
-    assert Asciidoctor::Lexer.send(:is_section_heading?, "=== AsciiDoc Home Page")
+    assert Asciidoctor::Lexer.is_section_heading?("AsciiDoc Home Page", "==================")
+    assert Asciidoctor::Lexer.is_section_heading?("=== AsciiDoc Home Page")
   end
 end
