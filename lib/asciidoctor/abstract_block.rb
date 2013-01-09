bdcf5228f809b6b67fc8ce4a400b21ab927c0196
Dan Allen <dan.j.allen@gmail.com>
2013-01-09 11:52:42 -0700

    section numbering
    - keep track of the index of each section as it's added to its parent
    - create a method for generating a section number
    - add section number support to backend templates
    - add convenience method to retrieve child sections from a parent block
    - tests!

--- lib/asciidoctor/abstract_block.rb
+++ lib/asciidoctor/abstract_block.rb
@@ -15,6 +15,7 @@ class Asciidoctor::AbstractBlock < Asciidoctor::AbstractNode
     @blocks = []
     @id = nil
     @level = (context == :document ? 0 : nil)
+    @next_section_index = 0 
   end
 
   # Public: Determine whether this Block contains block content
@@ -44,6 +45,58 @@ class Asciidoctor::AbstractBlock < Asciidoctor::AbstractNode
   #
   # Returns nothing.
   def <<(block)
+    if block.is_a?(Asciidoctor::Section)
+      assign_index(block)
+    end
     @blocks << block
+  end
+
+  # Public: Get the Array of child Section objects
+  #
+  # Only applies to Document and Section instances
+  #
+  # Examples
+  # 
+  #   section = Section.new(parent)
+  #   section << Block.new(section, :paragraph, 'paragraph 1')
+  #   section << Section.new(parent)
+  #   section << Block.new(section, :paragraph, 'paragraph 2')
+  #   section.sections.size
+  #   # => 1
+  #
+  # returns an Array of Section objects
+  def sections
+    @blocks.inject([]) {|collector, block|
+      collector << block if block.is_a?(Asciidoctor::Section)
+      collector
+    }
+  end
+
+  # Internal: Assign the next index (0-based) to this section
+  #
+  # Assign the next index of this section within the parent
+  # Block (in document order)
+  #
+  # returns nothing
+  def assign_index(section)
+    section.index = @next_section_index
+    @next_section_index += 1
+  end
+
+  # Internal: Reassign the section indexes
+  #
+  # Walk the descendents of the current Document or Section
+  # and reassign the section 0-based index value to each Section
+  # as it appears in document order.
+  # 
+  # returns nothing
+  def reindex_sections
+    @next_section_index = 0
+    @blocks.each {|block|
+      if block.is_a?(Asciidoctor::Section)
+        assign_index(block)
+        block.reindex_sections
+      end
+    }
   end
 end
