cce2d9010af3df69c859495b9e70ec67084df7e4
LightGuard <lightguard.jp@gmail.com>
2013-01-11 13:48:46 -0700

    Fixing string to symbol, attribute tests

--- test/option_test.rb
+++ test/option_test.rb
@@ -16,4 +16,22 @@ context 'Options' do
     assert_equal :book, options.doctype
     assert_equal 'my_input.asciidoc', options.input_file
   end
+
+  test 'standard attribute assignment' do
+    options = Asciidoctor::Cli::Options.parse!(%w"-a imagesdir=images,icons=1 my_input.asciidoc")
+
+    assert_equal 'images', options.attributes[:imagesdir]
+    assert_equal '1', options.attributes[:icons]
+  end
+
+  test 'multiple attribute arguments' do
+    options = Asciidoctor::Cli::Options.parse!(%w"-a imagesdir=images -a icons=1 my_input.asciidoc")
+
+    assert_equal 'images', options.attributes[:imagesdir]
+    assert_equal '1', options.attributes[:icons]
+  end
+
+  test 'accept STDIN instead of only a file name' do
+    pending "This hasn't been implemented yet in options"
+  end
 end
