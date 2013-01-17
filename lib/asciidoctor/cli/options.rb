8eccdc14959edff1878b534f6ad1b8a62a47ccde
LightGuard <lightguard.jp@gmail.com>
2013-01-16 21:18:56 -0700

    Make Options a subclass of hash to avoid massive changes
    Document now uses the Asciidoctor::Cli:Options class

--- lib/asciidoctor/cli/options.rb
+++ lib/asciidoctor/cli/options.rb
@@ -5,27 +5,19 @@ module Asciidoctor
   module Cli
 
     # Public: List of options that can be specified on the command line
-    class Options
+    class Options < Hash
 
-      attr_accessor :input_file
-      attr_accessor :output_file
-      attr_accessor :suppress_header_footer
-      attr_accessor :template_directory
-      attr_accessor :doctype
-      attr_accessor :help
-      attr_accessor :verbose
-      attr_accessor :attributes
-      attr_accessor :backend
-
-      def initialize()
-        @input_file = nil
-        @output_file = nil
-        @suppress_header_footer = false
-        @template_directory = nil
-        @doctype = :article
-        @verbose = false
-        @attributes = {}
-        @backend = :html5
+      def initialize(options = {})
+        self[:input_file] = options[:input_file]
+        self[:output_file] = options[:output_file]
+        self[:header_footer] = options.fetch(:header_footer, true)
+        self[:template_directory] = options[:template_directory]
+        self[:doctype] = options[:doctype] || :article
+        self[:verbose] = options.fetch(:verbose, false)
+        self[:attributes] = options[:attributes] || {}
+        self[:backend] = options[:backend] || :html5
+        self[:base_dir] = options[:base_dir] || Dir.pwd
+        self[:destination_dir] = options[:destination_dir]
       end
 
       def self.parse!(args)
@@ -37,28 +29,34 @@ module Asciidoctor
           opts.banner = "Usage: asciidoctor [options] input_file"
 
           opts.on('-v', '--verbose', 'Enable verbose mode') do |verbose|
-            self.verbose = true
+            self[:verbose] = true
           end
           opts.on('-d', '--doctype [DOCTYPE]', [:article, :book, :manpage],
                   'Document type to be used when rendering docbook: article, book, manpage. Default is article') do |doc_type|
-            self.doctype = doc_type
-            self.backend = :docbook
+            self[:doctype] = doc_type
+            self[:backend] = :docbook
           end
           opts.on('-o', '--output-file FILE', 'Output file') do |output_file|
-            self.output_file = output_file
+            self[:output_file] = output_file
           end
           opts.on('-s', '--no-headers-footer', 'Suppress the output of headers and footers') do |suppress_header_footer|
-            self.suppress_header_footer = true
+            self[:header_footer] = false
           end
           opts.on('-a', '--attribute key1=value,key2=value,keyN=value', Array,
                   'A list of attributes, key value pair separated by =, to override in the document') do |attribs|
             attribs.each do |attrib|
               tokens = attrib.split("=")
-              @attributes[tokens[0].to_sym] = tokens[1]
+              self[:attributes][tokens[0].to_sym] = tokens[1]
             end
           end
           opts.on('-t', '--template-dir DIR', 'Directory containing a non built in template to be used') do |template_dir|
-            self.template_directory = template_dir
+            self[:template_directory] = template_dir
+          end
+          opts.on('--base-dir DIR', 'Base directory containing the document and resources') do |base_dir|
+            self[:base_dir] = base_dir
+          end
+          opts.on('-D', '--destination-dir DIR', 'Destination output directory') do |dest_dir|
+            self[:destination_dir] = dest_dir
           end
 
           opts.on_tail("-h", "--help", "Show this message") do
@@ -75,8 +73,8 @@ module Asciidoctor
 
         begin
           opts.parse!(args)
-          self.input_file = args[0]
-          if self.input_file.nil? or self.input_file.empty?
+          self[:input_file] = args[0]
+          if self[:input_file].nil? or self[:input_file].empty?
             puts "Missing input file"
             puts opts
             exit
--- test/option_test.rb
+++ test/option_test.rb
@@ -1,5 +1,5 @@
 require 'test_helper'
-require "#{File.expand_path(File.dirname(__FILE__))}/../lib/asciidoctor/cli/options.rb"
+require "asciidoctor/cli/options"
 
 context 'Options' do
   test 'no input file option' do
@@ -11,24 +11,24 @@ context 'Options' do
   test 'basic argument assignment' do
     options = Asciidoctor::Cli::Options.parse!(%w"-v -s -d book my_input.asciidoc")
 
-    assert_equal true, options.verbose
-    assert_equal true, options.suppress_header_footer
-    assert_equal :book, options.doctype
-    assert_equal 'my_input.asciidoc', options.input_file
+    assert_equal true, options[:verbose]
+    assert_equal false, options[:header_footer]
+    assert_equal :book, options[:doctype]
+    assert_equal 'my_input.asciidoc', options[:input_file]
   end
 
   test 'standard attribute assignment' do
     options = Asciidoctor::Cli::Options.parse!(%w"-a imagesdir=images,icons=1 my_input.asciidoc")
 
-    assert_equal 'images', options.attributes[:imagesdir]
-    assert_equal '1', options.attributes[:icons]
+    assert_equal 'images', options[:attributes][:imagesdir]
+    assert_equal '1', options[:attributes][:icons]
   end
 
   test 'multiple attribute arguments' do
     options = Asciidoctor::Cli::Options.parse!(%w"-a imagesdir=images -a icons=1 my_input.asciidoc")
 
-    assert_equal 'images', options.attributes[:imagesdir]
-    assert_equal '1', options.attributes[:icons]
+    assert_equal 'images', options[:attributes][:imagesdir]
+    assert_equal '1', options[:attributes][:icons]
   end
 
   test 'accept STDIN instead of only a file name' do
