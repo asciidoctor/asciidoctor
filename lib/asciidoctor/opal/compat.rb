66e46ff31eaeec0bf02dfebfbed76be3a2b21876
Guillaume Grossetie <guillaume.grossetie@april-waf.com>
2013-10-02 13:42:59 +0200

    compat library for Opal

---
+++ lib/asciidoctor/opal/compat.rb
@@ -0,0 +1,40 @@
+class Array
+  def to_set
+    `#{self}`
+  end
+end
+
+class Set < Array
+end
+
+class File
+  SEPARATOR = '/'
+
+  def self.expand_path(path)
+    path
+  end
+
+  def self.join(*paths)
+    paths * SEPARATOR
+  end
+
+  def self.basename(path)
+    path[((path.rindex(File::SEPARATOR) || -1) + 1)..-1]
+  end
+
+  def self.dirname(path)
+    path[0..((path.rindex(SEPARATOR) || 0) - 1)]
+  end
+
+  def self.extname(path)
+    return '' if path.to_s.empty?
+    last_dot_idx = path[1..-1].rindex('.')
+    last_dot_idx.nil? ? "" : path[(last_dot_idx + 1)..-1]
+  end
+end
+
+class Dir
+  def self.pwd
+    '.'
+  end
+end
