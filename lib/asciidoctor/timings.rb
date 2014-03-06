70fe46bf5c28ac37afbab3b654432eeedb0c27ef
Dan Allen <dan.j.allen@gmail.com>
2014-03-05 17:50:29 -0700

    use internal hash as timings log

--- lib/asciidoctor/timings.rb
+++ lib/asciidoctor/timings.rb
@@ -1,8 +1,7 @@
 module Asciidoctor
   class Timings
-    attr_reader :read, :parse, :convert, :write
     def initialize
-      @read = @parse = @convert = @write = nil
+      @log = {}
       @timers = {}
     end
 
@@ -11,38 +10,30 @@ module Asciidoctor
     end
 
     def record key
-      instance_variable_set %(@#{key}), (::Time.now - (@timers.delete key))
+      @log[key] = (::Time.now - (@timers.delete key))
     end
 
     def read_parse
-      if @read || @parse
-        (@read || 0) + (@parse || 0)
-      else
-        nil
-      end
+      (time = (@log[:read] || 0) + (@log[:parse] || 0)) > 0 ? time : nil
+    end
+
+    def convert
+      @log[:convert]
     end
 
     def read_parse_convert
-      if @read || @parse || @convert
-        (@read || 0) + (@parse || 0) + (@convert || 0)
-      else
-        nil
-      end
+      (time = (@log[:read] || 0) + (@log[:parse] || 0) + (@log[:convert] || 0)) > 0 ? time : nil
     end
 
     def total
-      if @read || @parse || @convert || @write
-        (@read || 0) + (@parse || 0) + (@convert || 0) + (@write || 0)
-      else
-        nil
-      end
+      (time = (@log[:read] || 0) + (@log[:parse] || 0) + (@log[:convert] || 0) + (@log[:write] || 0)) > 0 ? time : nil
     end
 
     def print_report to = $stdout, subject = nil
       to.puts %(Input file: #{subject}) if subject
       to.puts %(  Time to read and parse source: #{'%05.5f' % read_parse})
-      to.puts %(  Time to convert document: #{@convert ? '%05.5f' % @convert : 'n/a'})
-      to.puts %(  Total time to read, parse and convert: #{'%05.5f' % read_parse_convert})
+      to.puts %(  Time to convert document: #{'%05.5f' % convert})
+      to.puts %(  Total time (read, parse and convert): #{'%05.5f' % read_parse_convert})
     end
   end
 end
