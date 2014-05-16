1919b8f2fcb27eecb6e95a9b877cd8c7d31813e6
Dan Allen <dan.j.allen@gmail.com>
2014-05-15 19:27:57 -0600

    resolves #836 and #846
    - use new Opal preprocessor guards (i.e., unless RUBY_ENGINE == 'opal')
    - use unicode escape sequence for passthrough placeholders and null character
    - fix several checks where a value is empty string instead of null in JavaScript
    - consolidate opal_ext require to a single line
    - move autoload declarations outside of Asciidoctor module block
    - use qualified name for top-level classes in AbstractNode

--- lib/asciidoctor/core_ext.rb
+++ lib/asciidoctor/core_ext.rb
@@ -1,7 +1,7 @@
 require 'asciidoctor/core_ext/object/nil_or_empty'
-# Opal barfs here if we use ::RUBY_VERSION_MIN_1_9
-unless RUBY_VERSION >= '1.9'
-  require 'asciidoctor/core_ext/string/chr'
-  # we append .to_s to keep Opal from processing the next require
-  require 'asciidoctor/core_ext/symbol/length'.to_s
+unless RUBY_ENGINE == 'opal'
+  unless RUBY_MIN_VERSION_1_9
+    require 'asciidoctor/core_ext/string/chr'
+    require 'asciidoctor/core_ext/symbol/length'
+  end
 end
