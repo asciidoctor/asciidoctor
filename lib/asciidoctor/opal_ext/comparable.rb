dd4886ecc6dfa184c3f699acd35707e8fcbc1983
Dan Allen <dan.j.allen@gmail.com>
2015-07-01 23:44:33 -0600

    resolves #1385 add implementation of != method to Opal
    - add implementation of !=
    - apply code styles

--- lib/asciidoctor/opal_ext/comparable.rb
+++ lib/asciidoctor/opal_ext/comparable.rb
@@ -1,38 +1,56 @@
-# workaround for an infinite loop in Opal 0.6.2 when comparing numbers
+class BasicObject
+  # Provides implementation for missing != method BasicObject. Allows the
+  # method :!= to be sent to an object.
+  def != other
+    `self !== other`
+  end
+end
+
+# workaround for an infinite loop in Opal 0.6.x when comparing numbers
 module Comparable
   def == other
     return true if equal? other
-    return false unless cmp = (self <=> other)
-    return `cmp == 0`
+    # if <=> returns nil, assume these objects can't be compared (and thus not equal)
+    return false unless res = (self <=> other)
+    return `res == 0`
   rescue StandardError
     false
   end
 
+  def != other
+    return false if equal? other
+    # if <=> returns nil, assume these objects can't be compared (and thus not equal)
+    return true unless res = (self <=> other)
+    return `res != 0`
+  rescue StandardError
+    true
+  end
+
   def > other
-    unless cmp = (self <=> other)
-      raise ArgumentError, "comparison of #{self.class} with #{other.class} failed"
+    unless res = (self <=> other)
+      raise ArgumentError, %(comparison of #{self.class} with #{other.class} failed)
     end
-    `cmp > 0`
+    `res > 0`
   end
 
   def >= other 
-    unless cmp = (self <=> other)
-      raise ArgumentError, "comparison of #{self.class} with #{other.class} failed"
+    unless res = (self <=> other)
+      raise ArgumentError, %(comparison of #{self.class} with #{other.class} failed)
     end
-    `cmp >= 0`
+    `res >= 0`
   end
   
   def < other
-    unless cmp = (self <=> other)
-      raise ArgumentError, "comparison of #{self.class} with #{other.class} failed"
+    unless res = (self <=> other)
+      raise ArgumentError, %(comparison of #{self.class} with #{other.class} failed)
     end
-    `cmp < 0`
+    `res < 0`
   end
 
   def <= other
-    unless cmp = (self <=> other)
-      raise ArgumentError, "comparison of #{self.class} with #{other.class} failed"
+    unless res = (self <=> other)
+      raise ArgumentError, %(comparison of #{self.class} with #{other.class} failed)
     end
-    `cmp <= 0`
+    `res <= 0`
   end
 end
