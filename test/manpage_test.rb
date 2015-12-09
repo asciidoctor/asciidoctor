31feac906f023ca413dc96040c3464815fa3bee1
Dorai Sitaram <ds26gte@yahoo.com>
2015-12-09 18:29:58 -0500

    resolves #1604 differentiate literal backslash from escape sequence
    - use escape character to differentiate formatting marks from literal backslash
    - escape all literal backslashes
    - use \0 for spaces instead of \<space> for output of UI macros
    - add tests for manpage backend
    - consolidate manify method

---
+++ test/manpage_test.rb
@@ -0,0 +1,70 @@
+# encoding: UTF-8
+unless defined? ASCIIDOCTOR_PROJECT_DIR
+  $: << File.dirname(__FILE__); $:.uniq!
+  require 'test_helper'
+end
+
+SAMPLE_MANPAGE_HEADER = <<-EOS.chomp
+= command (1)
+Author Name
+:doctype: manpage
+:man manual: Command Manual
+:man source: Command 1.2.3
+
+== NAME
+
+command - does stuff
+
+== SYNOPSIS
+
+*command* [_OPTION_]... _FILE_...
+
+== DESCRIPTION
+EOS
+
+context 'Manpage' do
+  context 'Manify' do
+    test 'should escape lone period' do
+      input = %(#{SAMPLE_MANPAGE_HEADER}
+
+.)
+      output = Asciidoctor.convert input, :backend => :manpage
+      assert_equal '\&.', output.lines.entries.last.chomp
+    end
+  end
+
+  context 'Backslash' do
+    test 'should not escape spaces for empty manual or source fields' do
+      input = SAMPLE_MANPAGE_HEADER.lines.select {|l| !l.start_with?(':man ') }
+      output = Asciidoctor.convert input, :backend => :manpage, :header_footer => true
+      assert_match ' Manual: \ \&', output
+      assert_match ' Source: \ \&', output
+      assert_match(/^\.TH "COMMAND" .* "\\ \\&" "\\ \\&"$/, output)
+    end
+
+    test 'should preserve backslashes in escape sequences' do
+      input = %(#{SAMPLE_MANPAGE_HEADER}
+
+"`hello`" '`goodbye`' *strong* _weak_ `even`)
+      output = Asciidoctor.convert input, :backend => :manpage
+      assert_equal '\(lqhello\(rq \(oqgoodbye\(cq \fBstrong\fP \fIweak\fP \f[CR]even\fP', output.lines.entries.last.chomp
+    end
+
+    test 'should escape backslashes in content' do
+      input = %(#{SAMPLE_MANPAGE_HEADER}
+
+\\.foo \\ bar\\
+baz)
+      output = Asciidoctor.convert input, :backend => :manpage
+      assert_equal '\(rs.foo \(rs bar\(rs', output.lines.entries[-2].chomp
+    end
+
+    test 'should escape literal escape sequence' do
+      input = %(#{SAMPLE_MANPAGE_HEADER}
+
+ \\fB makes text bold)
+      output = Asciidoctor.convert input, :backend => :manpage
+      assert_match '\(rsfB makes text bold', output
+    end
+  end
+end
