9072ef00dddabe8bf35369c9e3206d1b436f8b47
Dan Allen <dan.j.allen@gmail.com>
2016-08-27 23:57:27 -0600

    resolves #1815 add support for landscape orientation for tables in DocBook output
    - add orient="land" attribute to tables if either condition is true:
      * orientation attribute is landscape
      * rotate option is specified

--- lib/asciidoctor/converter/docbook5.rb
+++ lib/asciidoctor/converter/docbook5.rb
@@ -388,7 +388,7 @@ module Asciidoctor
       has_body = false
       result = []
       pgwide_attribute = (node.option? 'pgwide') ? ' pgwide="1"' : nil
-      result << %(<#{tag_name = node.title? ? 'table' : 'informaltable'}#{common_attributes node.id, node.role, node.reftext}#{pgwide_attribute} frame="#{node.attr 'frame', 'all'}" rowsep="#{['none', 'cols'].include?(node.attr 'grid') ? 0 : 1}" colsep="#{['none', 'rows'].include?(node.attr 'grid') ? 0 : 1}">)
+      result << %(<#{tag_name = node.title? ? 'table' : 'informaltable'}#{common_attributes node.id, node.role, node.reftext}#{pgwide_attribute} frame="#{node.attr 'frame', 'all'}" rowsep="#{['none', 'cols'].include?(node.attr 'grid') ? 0 : 1}" colsep="#{['none', 'rows'].include?(node.attr 'grid') ? 0 : 1}"#{(node.attr? 'orientation', 'landscape', nil) ? ' orient="land"' : nil}>)
       if (node.option? 'unbreakable')
         result << '<?dbfo keep-together="always"?>'
       elsif (node.option? 'breakable')
