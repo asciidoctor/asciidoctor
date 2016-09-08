885d8d854f9cdaa3e1dc06c5d945d59642729cb6
Dan Allen <dan.j.allen@gmail.com>
2016-09-07 19:00:58 -0600

    reorganize core library extensions

--- lib/asciidoctor/core_ext/nil_or_empty.rb
+++ lib/asciidoctor/core_ext/nil_or_empty.rb
@@ -1,6 +1,6 @@
 # A core library extension that defines the method nil_or_empty? as an alias to
 # optimize checks for nil? or empty? on common object types such as NilClass,
-# String, Array and Hash.
+# String, Array, Hash, and Numeric.
 
 class NilClass
   alias :nil_or_empty? :nil? unless method_defined? :nil_or_empty?
--- lib/asciidoctor/core_ext/string/limit.rb
+++ lib/asciidoctor/core_ext/string/limit.rb
@@ -1,35 +1,10 @@
-# Safely truncate the string to the specified number of bytes.
-# If a multibyte char gets split, the remaining fragment is removed.
-if RUBY_MIN_VERSION_1_9
-  class String
+class String
+  # Safely truncate the string to the specified number of bytes.
+  # If a multibyte char gets split, the dangling fragment is removed.
+  def limit size
+    return self unless size < bytesize
     # NOTE JRuby 1.7 & Rubinius fail to detect invalid encoding unless encoding is forced; impact is marginal.
-    def limit size
-      return self unless size < bytesize
-      size -= 1 until ((result = byteslice 0, size).force_encoding ::Encoding::UTF_8).valid_encoding?
-      result
-    end unless method_defined? :limit
-  end
-elsif RUBY_ENGINE_JRUBY
-  class String
-    def limit size
-      return self unless size < bytesize
-      result = (unpack %(a#{size}))[0]
-      begin
-        result.unpack 'U*'
-      rescue ArgumentError
-        result.chop!
-        retry
-      end
-      result
-    end unless method_defined? :limit
-  end
-else
-  class String
-    def limit size
-      return self unless size < bytesize
-      result = (unpack %(a#{size}))[0]
-      result.chop! until result.empty? || /.$/u =~ result
-      result
-    end unless method_defined? :limit
-  end
+    size -= 1 until ((result = byteslice 0, size).force_encoding ::Encoding::UTF_8).valid_encoding?
+    result
+  end unless method_defined? :limit
 end
