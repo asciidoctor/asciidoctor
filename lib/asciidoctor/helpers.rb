7b8b8692279559a83b884263b340557c561d856a
Dan Allen <dan.j.allen@gmail.com>
2017-04-29 00:23:52 -0600

    use raise with exception class instead of fail
    - fail is just an alias for raise
    - use a specific exception class for better flow control
    - use LoadError when raising from rescued LoadError
    - use NotImplementedError if converter is not set to bypass load rescue

--- lib/asciidoctor/helpers.rb
+++ lib/asciidoctor/helpers.rb
@@ -5,7 +5,7 @@ module Helpers
   #
   # Attempts to load the library specified in the first argument using the
   # Kernel#require. Rescues the LoadError if the library is not available and
-  # passes a message to Kernel#fail if on_failure is :abort or Kernel#warn if
+  # passes a message to Kernel#raise if on_failure is :abort or Kernel#warn if
   # on_failure is :warn to communicate to the user that processing is being
   # aborted or functionality is disabled, respectively. If a gem_name is
   # specified, the message communicates that a required gem is not installed.
@@ -17,7 +17,7 @@ module Helpers
   # on_failure - a Symbol that indicates how to handle a load failure (:abort, :warn, :ignore) (default: :abort)
   #
   # returns The return value of Kernel#require if the library is available and can be, or was previously, loaded.
-  # Otherwise, Kernel#fail is called with an appropriate message if on_failure is :abort.
+  # Otherwise, Kernel#raise is called with an appropriate message if on_failure is :abort.
   # Otherwise, Kernel#warn is called with an appropriate message and nil returned if on_failure is :warn.
   # Otherwise, nil is returned.
   def self.require_library name, gem_name = true, on_failure = :abort
@@ -27,14 +27,14 @@ module Helpers
       gem_name = name if gem_name == true
       case on_failure
       when :abort
-        fail %(asciidoctor: FAILED: required gem '#{gem_name}' is not installed. Processing aborted.)
+        raise ::LoadError, %(asciidoctor: FAILED: required gem '#{gem_name}' is not installed. Processing aborted.)
       when :warn
         warn %(asciidoctor: WARNING: optional gem '#{gem_name}' is not installed. Functionality disabled.)
       end
     else
       case on_failure
       when :abort
-        fail %(asciidoctor: FAILED: #{e.message.chomp '.'}. Processing aborted.)
+        raise ::LoadError, %(asciidoctor: FAILED: #{e.message.chomp '.'}. Processing aborted.)
       when :warn
         warn %(asciidoctor: WARNING: #{e.message.chomp '.'}. Functionality disabled.)
       end
