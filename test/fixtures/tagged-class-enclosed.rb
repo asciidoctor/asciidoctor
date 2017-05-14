382d35318102e8c986d082512911f7e98fa77a30
Dan Allen <dan.j.allen@gmail.com>
2017-05-14 03:52:09 -0600

    resolves #1516 add option to exclude tags when including file
    - in addition to including tags, add option to exclude tags using !name syntax
    - introduce wildcard for including all (*) or excluding all (!*) tags
    - introduce wildcard for selecting non-tagged lines (**)
    - don't select nested tags with wildcard if ancestor tag is excluded
    - warn if a mismatched or unexpected end tag is found
    - improve efficiency of detecting and processing tag directives
    - maintain a tag stack for handling nested tags
    - skip call to push_include if selected lines array is empty
    - ignore tag and tags attribute if empty (previously caused an infinite loop)
    - add tests for excluding tags

---
+++ test/fixtures/tagged-class-enclosed.rb
@@ -0,0 +1,26 @@
+#tag::all[]
+class Dog
+  #tag::init[]
+  def initialize breed
+    @breed = breed
+  end
+  #end::init[]
+  #tag::bark[]
+
+  def bark
+    #tag::bark-beagle[]
+    if @breed == 'beagle'
+      'woof woof woof woof woof'
+    #end::bark-beagle[]
+    #tag::bark-other[]
+    else
+      'woof woof'
+    #end::bark-other[]
+    #tag::bark-all[]
+    #tag::bark-all[]
+    end
+    #end::bark-all[]
+  end
+  #end::bark[]
+end
+#end::all[]
---
+++ test/fixtures/tagged-class.rb
@@ -0,0 +1,23 @@
+class Dog
+  #tag::init[]
+  def initialize breed
+    @breed = breed
+  end
+  #end::init[]
+  #tag::bark[]
+
+  def bark
+    #tag::bark-beagle[]
+    if @breed == 'beagle'
+      'woof woof woof woof woof'
+    #end::bark-beagle[]
+    #tag::bark-other[]
+    else
+      'woof woof'
+    #end::bark-other[]
+    #tag::bark-all[]
+    end
+    #end::bark-all[]
+  end
+  #end::bark[]
+end
