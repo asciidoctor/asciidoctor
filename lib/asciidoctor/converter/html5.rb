965cb05e0801b27eeac3b0b86a18b365228156ef
Dan Allen <dan.j.allen@gmail.com>
2017-05-17 23:08:35 -0600

    resolves #2089 include docfile in warning when stylesheet cannot be read (PR #2201)
    - add docfile to warning message emitted when stylesheet cannot be read
    - warn when stylesheet to copy cannot be read
    - allow label to be passed to read_asset method
    - refer to missing file as a stylesheet

--- lib/asciidoctor/converter/html5.rb
+++ lib/asciidoctor/converter/html5.rb
@@ -75,7 +75,7 @@ module Asciidoctor
           result << %(<link rel="stylesheet" href="#{node.normalize_web_path((node.attr 'stylesheet'), (node.attr 'stylesdir', ''))}"#{slash}>)
         else
           result << %(<style>
-#{node.read_asset node.normalize_system_path((node.attr 'stylesheet'), (node.attr 'stylesdir', '')), :warn_on_failure => true}
+#{node.read_asset node.normalize_system_path((node.attr 'stylesheet'), (node.attr 'stylesdir', '')), :warn_on_failure => true, :label => 'stylesheet'}
 </style>)
         end
       end
