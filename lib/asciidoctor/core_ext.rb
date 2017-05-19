cbf18b7717d5d695154f5c1ede3d5c0e99f6fbf0
Dan Allen <dan.j.allen@gmail.com>
2017-05-18 23:57:15 -0600

    backport IO.write to Opal until fixed upstream

--- lib/asciidoctor/core_ext.rb
+++ lib/asciidoctor/core_ext.rb
@@ -2,6 +2,7 @@ require 'asciidoctor/core_ext/nil_or_empty'
 require 'asciidoctor/core_ext/regexp/is_match'
 if RUBY_MIN_VERSION_1_9
   require 'asciidoctor/core_ext/string/limit_bytesize'
+  require 'asciidoctor/core_ext/1.8.7/io/write' if RUBY_ENGINE == 'opal'
 elsif RUBY_ENGINE != 'opal'
   require 'asciidoctor/core_ext/1.8.7/io/binread'
   require 'asciidoctor/core_ext/1.8.7/io/write'
