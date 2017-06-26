c733d788148d23b0dea0ada921665514dd3a120d
Dan Allen <dan.j.allen@gmail.com>
2017-06-25 20:06:19 -0600

    configure run-tests.sh script to run all tests

--- run-tests.sh
+++ run-tests.sh
@@ -6,5 +6,5 @@
 if [ "$1" = "all" ]; then
   rvm 1.8@asciidoctor-dev,jruby@asciidoctor-dev,rbx@asciidoctor-dev,1.9@asciidoctor-dev,2.0@asciidoctor-dev,2.1@asciidoctor-dev "do" ./run-tests.sh
 else
-  rake > /tmp/asciidoctor-test-results.txt 2>&1; cat /tmp/asciidoctor-test-results.txt
+  rake test:all > /tmp/asciidoctor-test-results.txt 2>&1; cat /tmp/asciidoctor-test-results.txt
 fi
