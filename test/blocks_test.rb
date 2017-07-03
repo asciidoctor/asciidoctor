463fe0e4d750369bf478cc44a86d93ba4e918888
Dan Allen <dan.j.allen@gmail.com>
2017-07-03 00:08:33 -0600

    upgrade highlight.js to 9.12.0

--- test/blocks_test.rb
+++ test/blocks_test.rb
@@ -2347,7 +2347,7 @@ puts "AsciiDoc, FTW!"
 
       output = render_string input, :safe => Asciidoctor::SafeMode::SAFE
       assert_css 'html > head > link[rel="stylesheet"][href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css"]', output, 1
-      assert_css 'html > body > script[src="http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/highlight.min.js"]', output, 1
+      assert_css 'html > body > script[src="http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"]', output, 1
     end
 
     test 'should use no uri scheme for assets when asset-uri-scheme is blank' do
@@ -2364,7 +2364,7 @@ puts "AsciiDoc, FTW!"
 
       output = render_string input, :safe => Asciidoctor::SafeMode::SAFE
       assert_css 'html > head > link[rel="stylesheet"][href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css"]', output, 1
-      assert_css 'html > body > script[src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/highlight.min.js"]', output, 1
+      assert_css 'html > body > script[src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"]', output, 1
     end
   end
 
