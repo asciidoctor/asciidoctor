e58beba7d621cdc85baf7d9d3bf0644ddf4cde3b
Dan Allen <dan.j.allen@gmail.com>
2017-07-18 23:53:38 -0600

    remove useless group in revision line regexp

--- lib/asciidoctor.rb
+++ lib/asciidoctor.rb
@@ -434,7 +434,7 @@ module Asciidoctor
     #   v1.0, 2013-01-01: Ring in the new year release
     #   1.0, Jan 01, 2013
     #
-    RevisionInfoLineRx = /^(?:\D*(.*?),)?(?: *(?!:)(.*?))(?: *(?!^): *(.*))?$/
+    RevisionInfoLineRx = /^(?:\D*(.*?),)? *(?!:)(.*?)(?: *(?!^): *(.*))?$/
 
     # Matches the title and volnum in the manpage doctype.
     #
