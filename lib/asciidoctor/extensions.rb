b467bb88a4bd03b67b8e63064664caea9fe95340
Dan Allen <dan.j.allen@gmail.com>
2017-08-17 23:58:44 -0600

    combine assignments in Registry constructor

--- lib/asciidoctor/extensions.rb
+++ lib/asciidoctor/extensions.rb
@@ -648,8 +648,7 @@ module Extensions
 
     def initialize groups = {}
       @groups = groups
-      @preprocessor_extensions = @tree_processor_extensions = @postprocessor_extensions = @include_processor_extensions = @docinfo_processor_extensions = nil
-      @block_extensions = @block_macro_extensions = @inline_macro_extensions = nil
+      @preprocessor_extensions = @tree_processor_extensions = @postprocessor_extensions = @include_processor_extensions = @docinfo_processor_extensions = @block_extensions = @block_macro_extensions = @inline_macro_extensions = nil
       @document = nil
     end
 
