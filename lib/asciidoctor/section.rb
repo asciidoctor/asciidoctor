606d2efdae70169565bf9a27ebf99e463339523e
Dan Allen <dan.j.allen@gmail.com>
2018-04-22 23:58:01 -0600

    optimize order of conditions in Section#sectnum

--- lib/asciidoctor/section.rb
+++ lib/asciidoctor/section.rb
@@ -110,12 +110,12 @@ class Section < AbstractBlock
   # Returns the section number as a String
   def sectnum(delimiter = '.', append = nil)
     append ||= (append == false ? '' : delimiter)
-    if @level == 0
-      %(#{Helpers.int_to_roman @number}#{append})
-    elsif @level > 1 && Section === @parent
-      %(#{@parent.sectnum(delimiter)}#{@number}#{append})
-    else
+    if @level == 1
       %(#{@number}#{append})
+    elsif @level > 1
+      Section === @parent ? %(#{@parent.sectnum(delimiter)}#{@number}#{append}) : %(#{@number}#{append})
+    else # @level == 0
+      %(#{Helpers.int_to_roman @number}#{append})
     end
   end
 
