0cf80a34db476af67af0e96e7788e81ca405323c
Dan Allen <dan.j.allen@gmail.com>
2018-04-26 20:51:58 -0600

    store document in local variable in sub_inline_xrefs

--- lib/asciidoctor/substitutors.rb
+++ lib/asciidoctor/substitutors.rb
@@ -1014,7 +1014,7 @@ module Substitutors
         if m[0].start_with? RS
           next m[0][1..-1]
         end
-        attrs = {}
+        attrs, doc = {}, @document
         if (refid = m[1])
           refid, text = refid.split ',', 2
           text = text.lstrip if text
@@ -1023,7 +1023,7 @@ module Substitutors
           if (text = m[3])
             text = text.gsub ESC_R_SB, R_SB if text.include? R_SB
             # NOTE if an equal sign (=) is present, parse text as attributes
-            text = ((AttributeList.new text, self).parse_into attrs)[1] if (text.include? '=') && !@document.compat_mode
+            text = ((AttributeList.new text, self).parse_into attrs)[1] if (text.include? '=') && !doc.compat_mode
           end
         end
 
@@ -1049,29 +1049,29 @@ module Substitutors
         # handles: #id
         if target
           refid = fragment
-          logger.warn %(invalid reference: #{refid}) if $VERBOSE && !(@document.catalog[:ids].key? refid)
+          logger.warn %(invalid reference: #{refid}) if $VERBOSE && !(doc.catalog[:ids].key? refid)
         # handles: path#, path.adoc#, path#id, path.adoc#id, or path (from path.adoc)
         elsif path
           # the referenced path is this document, or its contents has been included in this document
-          if @document.attributes['docname'] == path || @document.catalog[:includes][path]
+          if doc.attributes['docname'] == path || doc.catalog[:includes][path]
             if fragment
               refid, path, target = fragment, nil, %(##{fragment})
-              logger.warn %(invalid reference: #{refid}) if $VERBOSE && !(@document.catalog[:ids].key? refid)
+              logger.warn %(invalid reference: #{refid}) if $VERBOSE && !(doc.catalog[:ids].key? refid)
             else
               refid, path, target = nil, nil, '#'
             end
           else
             refid = fragment ? %(#{path}##{fragment}) : path
-            path = %(#{@document.attributes['relfileprefix']}#{path}#{@document.attributes.fetch 'outfilesuffix', '.html'})
+            path = %(#{doc.attributes['relfileprefix']}#{path}#{doc.attributes.fetch 'outfilesuffix', '.html'})
             target = fragment ? %(#{path}##{fragment}) : path
           end
         # handles: id or Node Title or Reference Text
         else
           # do reverse lookup on fragment if not a known ID and resembles reftext (contains a space or uppercase char)
-          unless @document.catalog[:ids].key? fragment
-            if Compliance.natural_xrefs && !@document.compat_mode &&
+          unless doc.catalog[:ids].key? fragment
+            if Compliance.natural_xrefs && !doc.compat_mode &&
                 ((fragment.include? ' ') || fragment.downcase != fragment) &&
-                (resolved_id = @document.catalog[:ids].key fragment)
+                (resolved_id = doc.catalog[:ids].key fragment)
               fragment = resolved_id
             elsif $VERBOSE
               logger.warn %(invalid reference: #{fragment})
