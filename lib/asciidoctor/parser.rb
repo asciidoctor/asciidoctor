52125d09c672b01d195862bfb47a92465100095b
Owen Heisler <owenh000@gmail.com>
2018-07-16 18:19:51 -0500

    Catalog anchors in ordered list items

--- lib/asciidoctor/parser.rb
+++ lib/asciidoctor/parser.rb
@@ -1309,6 +1309,17 @@ class Parser
         end
       elsif list_type == :olist
         list_item.marker = (sibling_trait ||= resolve_ordered_list_marker(match[1], list_block.items.size, true, reader))
+        if item_text.start_with?('[')
+          if style && style == 'bibliography'
+            if InlineBiblioAnchorRx =~ item_text
+              catalog_inline_biblio_anchor $1, $2, list_item, reader
+            end
+          elsif item_text.start_with?('[[')
+            if LeadingInlineAnchorRx =~ item_text
+              catalog_inline_anchor $1, $2, list_item, reader
+            end
+          end
+        end
       else # :colist
         list_item.marker = (sibling_trait ||= '<1>')
       end
