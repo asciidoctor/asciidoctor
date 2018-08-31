ccc029eadb1ec92b3bfed73dc3aff742a786a668
Dan Allen <dan.j.allen@gmail.com>
2018-08-30 23:45:04 -0600

    use slice with explicit arguments instead of range slice to take a substring

--- lib/asciidoctor/converter.rb
+++ lib/asciidoctor/converter.rb
@@ -77,7 +77,7 @@ module Asciidoctor
         raise ::ArgumentError, %(Cannot determine backend for converter: #{self.class}) unless @backend
         base = @backend.sub TrailingDigitsRx, ''
         if (ext = DEFAULT_EXTENSIONS[base])
-          type = ext[1..-1]
+          type = ext.slice 1, ext.length
         else
           # QUESTION should we be forcing the basebackend to html if unknown?
           base = 'html'
