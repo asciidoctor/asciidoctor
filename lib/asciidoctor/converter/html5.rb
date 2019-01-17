b6bca6a718472abb52edadcf3c7c01c295d8e955
Dan Allen <dan.j.allen@gmail.com>
2019-01-16 23:46:50 -0700

    resolves #3014 add support for the mute option on vimeo videos

--- lib/asciidoctor/converter/html5.rb
+++ lib/asciidoctor/converter/html5.rb
@@ -974,17 +974,13 @@ Your browser does not support the audio tag.
           asset_uri_scheme = %(#{asset_uri_scheme}:)
         end
         start_anchor = (node.attr? 'start', nil, false) ? %(#at=#{node.attr 'start'}) : ''
-        delimiter = '?'
-        if node.option? 'autoplay'
-          autoplay_param = %(#{delimiter}autoplay=1)
-          delimiter = '&amp;'
-        else
-          autoplay_param = ''
-        end
-        loop_param = (node.option? 'loop') ? %(#{delimiter}loop=1) : ''
+        delimiter = ['?']
+        autoplay_param = (node.option? 'autoplay') ? %(#{delimiter.pop || '&amp;'}autoplay=1) : ''
+        loop_param = (node.option? 'loop') ? %(#{delimiter.pop || '&amp;'}loop=1) : ''
+        muted_param = (node.option? 'mute') ? %(#{delimiter.pop || '&amp;'}muted=1) : ''
         %(<div#{id_attribute}#{class_attribute}>#{title_element}
 <div class="content">
-<iframe#{width_attribute}#{height_attribute} src="#{asset_uri_scheme}//player.vimeo.com/video/#{node.attr 'target'}#{start_anchor}#{autoplay_param}#{loop_param}" frameborder="0"#{(node.option? 'nofullscreen') ? '' : (append_boolean_attribute 'allowfullscreen', xml)}></iframe>
+<iframe#{width_attribute}#{height_attribute} src="#{asset_uri_scheme}//player.vimeo.com/video/#{node.attr 'target'}#{start_anchor}#{autoplay_param}#{loop_param}#{muted_param}" frameborder="0"#{(node.option? 'nofullscreen') ? '' : (append_boolean_attribute 'allowfullscreen', xml)}></iframe>
 </div>
 </div>)
       when 'youtube'
