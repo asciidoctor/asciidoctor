4bcb71fdec5b688e87177138ea9902277cd33104
Dan Allen <dan.j.allen@gmail.com>
2019-03-11 03:10:42 -0600

    allow failure level to be set to DEBUG

--- lib/asciidoctor/cli/options.rb
+++ lib/asciidoctor/cli/options.rb
@@ -118,7 +118,7 @@ module Asciidoctor
               'may be specified more than once') do |path|
             (self[:requires] ||= []).concat(path.split ',')
           end
-          opts.on('--failure-level LEVEL', %w(warning WARNING error ERROR), 'set minimum logging level that triggers a non-zero exit code: [WARN, ERROR] (default: FATAL)') do |level|
+          opts.on('--failure-level LEVEL', %w(warning WARNING error ERROR debug DEBUG), 'set minimum logging level that triggers non-zero exit code: [WARN, ERROR, DEBUG] (default: FATAL)') do |level|
             level = 'WARN' if (level = level.upcase) == 'WARNING'
             self[:failure_level] = ::Logger::Severity.const_get level, false
           end
