23d8c6f40c64ff94e47c2ad7d95257d87d96053c
Dan Allen <dan.j.allen@gmail.com>
2019-03-31 21:30:46 -0600

    don't require core_ext patches on Opal that aren't needed

--- lib/asciidoctor/core_ext.rb
+++ lib/asciidoctor/core_ext.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
-require_relative 'core_ext/float/truncate'
-require_relative 'core_ext/hash/merge'
-require_relative 'core_ext/match_data/names' if RUBY_ENGINE == 'opal'
 require_relative 'core_ext/nil_or_empty'
-require_relative 'core_ext/regexp/is_match'
+require_relative 'core_ext/hash/merge'
+if RUBY_ENGINE == 'opal'
+  require_relative 'core_ext/match_data/names'
+else
+  require_relative 'core_ext/float/truncate'
+  require_relative 'core_ext/regexp/is_match'
+end
--- lib/asciidoctor/core_ext/match_data/names.rb
+++ lib/asciidoctor/core_ext/match_data/names.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-# NOTE remove once implemented in Opal
+# NOTE remove once implemented in Opal; see https://github.com/opal/opal/issues/1964
 class MatchData
   def names
     []
