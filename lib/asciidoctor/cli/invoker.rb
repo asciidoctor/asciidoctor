e26d7fecad5f71068821d517d5da23bdd54e0dad
Dan Allen <dan.j.allen@gmail.com>
2020-01-12 04:58:01 -0700

    fix source date epoch tests in Ruby 2.7

--- lib/asciidoctor/cli/invoker.rb
+++ lib/asciidoctor/cli/invoker.rb
@@ -42,6 +42,8 @@ module Asciidoctor
         non_posix_env = ::File::ALT_SEPARATOR == RS
         err = @err || $stderr
         show_timings = false
+        # NOTE in Ruby 2.7, RubyGems sets SOURCE_DATE_EPOCH if it's not set
+        ENV.delete 'SOURCE_DATE_EPOCH' if ENV.key? 'DELETE_SOURCE_DATE_EPOCH'
 
         @options.map do |key, val|
           case key
