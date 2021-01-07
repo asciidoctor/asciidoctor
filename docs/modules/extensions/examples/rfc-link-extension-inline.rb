a2cde9e616576f7c07102bc7971e13d8122fa3c7
Daniel Mulholland <dan.mulholland@gmail.com>
2021-01-07 13:59:53 +1300

    Mention named capture groups, prefer single quotes

--- docs/modules/extensions/examples/rfc-link-extension-inline.rb
+++ docs/modules/extensions/examples/rfc-link-extension-inline.rb
@@ -7,11 +7,14 @@ class RFCStandardLinkInlineMacro < Asciidoctor::Extensions::InlineMacroProcessor
 
   named :rfc_standard_link
   match /\bRFC ?([0-9]{1,4})/
+  # named capture groups called _only_ target and content are allowed.
+  # The above could also be written alternatively:
+  #  match /(\b)(?<target>TP.[A-Z]{2} [0-9]{2}.[0-9]{2})/
   parse_content_as :text
 
   def process(parent, target, attrs)
-    base_path = 'https://tools.ietf.org/html/'
-    create_anchor parent, target, type: :link, target: base_path + target.gsub(/\s+/, '')
+    base_path = "https://tools.ietf.org/html/"
+    create_anchor parent, target, type: :link, target: base_path + target.gsub(/\s+/, "")
   end
 end
 # end::extension[]
@@ -21,10 +24,10 @@ Asciidoctor::Extensions.register do
   inline_macro RFCStandardLinkInlineMacro
 end
 
-source = '= Standards are Funny
+source = "= Standards are Funny
 
 Check out for example RFC 1882 at Christmas, RFC1927 when I forget the space.
-Not to mention RFC 2549, a personal favorite.'
+Not to mention RFC 2549, a personal favorite."
 
 doc = Asciidoctor.load source
 
