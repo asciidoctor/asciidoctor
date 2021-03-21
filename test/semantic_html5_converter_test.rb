b03c2bc2808873412d490525e064d1829d50e137
Guillaume Grossetie <ggrossetie@gmail.com>
2021-03-21 12:31:51 +0100

    Remove alias sem-html5 as semantic-html5 is an interim name

--- test/semantic_html5_converter_test.rb
+++ test/semantic_html5_converter_test.rb
@@ -2,7 +2,7 @@
 require_relative 'test_helper'
 
 context 'Semantic HTML 5 converter' do
-  (Dir.glob "#{ASCIIDOCTOR_TEST_DIR}/fixtures/sem-html5-scenarios/*.adoc").each do |input_filename|
+  (Dir.glob "#{ASCIIDOCTOR_TEST_DIR}/fixtures/semantic-html5-scenarios/*.adoc").each do |input_filename|
     input_stem = input_filename.slice 0, input_filename.length - 5
     scenario_name = input_stem.gsub '/', '::'
     input_filename = File.absolute_path input_filename
@@ -10,7 +10,7 @@ context 'Semantic HTML 5 converter' do
     test scenario_name do
       input = IO.read input_filename, mode: 'r:UTF-8', newline: :universal
       expected = (IO.read output_filename, mode: 'r:UTF-8', newline: :universal).chomp
-      result = (convert_string_to_embedded input, backend: 'sem-html5')
+      result = (convert_string_to_embedded input, backend: 'semantic-html5')
       assert_equal expected, result
     end
   end
