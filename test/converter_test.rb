e4ea846cbb4ace53f8f1f4530d791557d829323b
Dan Allen <dan.j.allen@gmail.com>
2021-04-30 02:56:00 -0600

    ignore redundant root namespace reference when checking for Tilt in test suite

--- test/converter_test.rb
+++ test/converter_test.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'test_helper'
-require 'tilt' unless defined? ::Tilt.new
+require 'tilt' unless defined? Tilt.new
 
 context 'Converter' do
   context 'View options' do
