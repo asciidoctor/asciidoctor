ca304b110ddbd03b3b7baa70dffa4afc2b3d45bf
Dan Allen <dan.j.allen@gmail.com>
2021-05-07 01:43:47 -0600

    consistently use += for self assignment

--- lib/asciidoctor/substitutors.rb
+++ lib/asciidoctor/substitutors.rb
@@ -1534,7 +1534,7 @@ module Substitutors
         case c
         when ','
           if quote_open
-            accum = accum + c
+            accum += c
           else
             values << accum.strip
             accum = ''
@@ -1542,7 +1542,7 @@ module Substitutors
         when '"'
           quote_open = !quote_open
         else
-          accum = accum + c
+          accum += c
         end
       end
       values << accum.strip
