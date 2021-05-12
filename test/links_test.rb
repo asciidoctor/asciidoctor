5f63c012834480896452f41d9ca494c75c006c93
Dan Allen <dan.j.allen@gmail.com>
2021-05-11 23:59:51 -0600

    add basic test for automatic link text for xref macro shorthand

--- test/links_test.rb
+++ test/links_test.rb
@@ -475,6 +475,16 @@ context 'Links' do
     assert_xpath '//a[@href="#tigers"][text() = "About Tigers"]', convert_string(input), 1
   end
 
+  test 'xref should use title of target as link text when no explicit reftext is specified' do
+    input = <<~'EOS'
+    <<tigers>>
+
+    [#tigers]
+    == Tigers
+    EOS
+    assert_xpath '//a[@href="#tigers"][text() = "Tigers"]', convert_string(input), 1
+  end
+
   test 'xref using angled bracket syntax with quoted label' do
     input = <<~'EOS'
     <<tigers,"About Tigers">>
