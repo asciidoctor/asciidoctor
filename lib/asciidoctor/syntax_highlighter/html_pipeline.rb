97e64790025c7847326fa5ee89edcb4c9c3ece2b
Dan Allen <dan.j.allen@gmail.com>
2021-05-18 00:35:11 -0600

    use %() instead of %{} for an interpolated string inside an expression inside an interpolated string

--- lib/asciidoctor/syntax_highlighter/html_pipeline.rb
+++ lib/asciidoctor/syntax_highlighter/html_pipeline.rb
@@ -5,7 +5,7 @@ class SyntaxHighlighter::HtmlPipelineAdapter < SyntaxHighlighter::Base
   register_for 'html-pipeline'
 
   def format node, lang, opts
-    %(<pre#{lang ? %{ lang="#{lang}"} : ''}><code>#{node.content}</code></pre>)
+    %(<pre#{lang ? %( lang="#{lang}") : ''}><code>#{node.content}</code></pre>)
   end
 end
 end
