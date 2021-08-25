e3eb919376eeaa274e25a8e878cc0c22b5601559
Robert Bennett <rltbennett@icloud.com>
2021-08-25 00:04:53 -0400

    Added example of pointing Pygments adapter to one's own stylesheet

---
+++ docs/modules/syntax-highlighting/examples/custom-theme-pygments-syntax-highlighter.rb
@@ -0,0 +1,12 @@
+class MyPygmentsAdapter < (Asciidoctor::SyntaxHighlighter.for 'pygments')
+	register_for :pygments
+
+	def write_stylesheet? doc
+		false
+	end
+
+	def docinfo location, doc, opts
+		slash = opts[:self_closing_tag_slash]
+		%(<link rel="stylesheet" href="/styles/syntax-theme.css"#{slash}>)
+	end
+end
