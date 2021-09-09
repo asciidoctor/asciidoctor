4acaa9ebad731e303adf8969f64e984756c966bf
Dan Allen <dan.j.allen@gmail.com>
2021-09-09 03:15:40 -0600

    backport fix for #4156 include primary video in value of playlist attribute when embedding YouTube video

--- lib/asciidoctor/converter/html5.rb
+++ lib/asciidoctor/converter/html5.rb
@@ -1075,7 +1075,7 @@ Your browser does not support the audio tag.
         target, playlist = target.split ',', 2
         if (playlist ||= (node.attr 'playlist'))
           # INFO playlist bar doesn't appear in Firefox unless showinfo=1 and modestbranding=1
-          list_param = %(&amp;playlist=#{playlist})
+          list_param = %(&amp;playlist=#{target},#{playlist})
         else
           # NOTE for loop to work, playlist must be specified; use VIDEO_ID if there's no explicit playlist
           list_param = has_loop_param ? %(&amp;playlist=#{target}) : ''
