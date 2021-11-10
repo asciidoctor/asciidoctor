c342eb2c74ec1b5c0cafccede9db7dbc72fbfe1a
Dan Allen <dan.j.allen@gmail.com>
2021-11-09 23:52:43 -0700

    rewrite convert handler for inline image to make it more clear and efficient
    - isolate logic for converting icon
    - simplify how attributes are assembled
    - untangle logic

--- lib/asciidoctor/converter/html5.rb
+++ lib/asciidoctor/converter/html5.rb
@@ -1185,46 +1185,46 @@ Your browser does not support the video tag.
   end
 
   def convert_inline_image node
-    if (type = node.type || 'image') == 'icon' && (node.document.attr? 'icons', 'font')
-      class_attr_val = %(fa fa-#{node.target})
-      { 'size' => 'fa-', 'rotate' => 'fa-rotate-', 'flip' => 'fa-flip-' }.each do |key, prefix|
-        class_attr_val = %(#{class_attr_val} #{prefix}#{node.attr key}) if node.attr? key
+    target = node.target
+    if (type = node.type || 'image') == 'icon'
+      if (icons = node.document.attr 'icons') == 'font'
+        i_class_attr_val = %(fa fa-#{target})
+        { 'size' => 'fa-', 'rotate' => 'fa-rotate-', 'flip' => 'fa-flip-' }.each do |key, prefix|
+          i_class_attr_val = %(#{i_class_attr_val} #{prefix}#{node.attr key}) if node.attr? key
+        end
+        attrs = (node.attr? 'title') ? %( title="#{node.attr 'title'}") : ''
+        img = %(<i class="#{i_class_attr_val}"#{attrs}></i>)
+      elsif icons
+        attrs = (node.attr? 'width') ? %( width="#{node.attr 'width'}") : ''
+        attrs = %(#{attrs} height="#{node.attr 'height'}") if node.attr? 'height'
+        attrs = %(#{attrs} title="#{node.attr 'title'}") if node.attr? 'title'
+        img = %(<img src="#{node.icon_uri target}" alt="#{encode_attribute_value node.alt}"#{attrs}#{@void_element_slash}>)
+      else
+        img = %([#{node.alt}&#93;)
       end
-      title_attr = (node.attr? 'title') ? %( title="#{node.attr 'title'}") : ''
-      img = %(<i class="#{class_attr_val}"#{title_attr}></i>)
-    elsif type == 'icon' && !(node.document.attr? 'icons')
-      img = %([#{node.alt}&#93;)
     else
-      target = node.target
-      attrs = []
-      attrs << %( width="#{node.attr 'width'}") if node.attr? 'width'
-      attrs << %( height="#{node.attr 'height'}") if node.attr? 'height'
-      attrs << %( title="#{node.attr 'title'}") if node.attr? 'title'
-      attrs = attrs.empty? ? '' : attrs.join
-      if type != 'icon' && ((node.attr? 'format', 'svg') || (target.include? '.svg')) && node.document.safe < SafeMode::SECURE
+      attrs = (node.attr? 'width') ? %( width="#{node.attr 'width'}") : ''
+      attrs = %(#{attrs} height="#{node.attr 'height'}") if node.attr? 'height'
+      attrs = %(#{attrs} title="#{node.attr 'title'}") if node.attr? 'title'
+      if ((node.attr? 'format', 'svg') || (target.include? '.svg')) && node.document.safe < SafeMode::SECURE
         if node.option? 'inline'
           img = (read_svg_contents node, target) || %(<span class="alt">#{node.alt}</span>)
         elsif node.option? 'interactive'
           fallback = (node.attr? 'fallback') ? %(<img src="#{node.image_uri node.attr 'fallback'}" alt="#{encode_attribute_value node.alt}"#{attrs}#{@void_element_slash}>) : %(<span class="alt">#{node.alt}</span>)
           img = %(<object type="image/svg+xml" data="#{node.image_uri target}"#{attrs}>#{fallback}</object>)
         else
-          img = %(<img src="#{type == 'icon' ? (node.icon_uri target) : (node.image_uri target)}" alt="#{encode_attribute_value node.alt}"#{attrs}#{@void_element_slash}>)
+          img = %(<img src="#{node.image_uri target}" alt="#{encode_attribute_value node.alt}"#{attrs}#{@void_element_slash}>)
         end
       else
-        img = %(<img src="#{type == 'icon' ? (node.icon_uri target) : (node.image_uri target)}" alt="#{encode_attribute_value node.alt}"#{attrs}#{@void_element_slash}>)
+        img = %(<img src="#{node.image_uri target}" alt="#{encode_attribute_value node.alt}"#{attrs}#{@void_element_slash}>)
       end
     end
     img = %(<a class="image" href="#{node.attr 'link'}"#{(append_link_constraint_attrs node).join}>#{img}</a>) if node.attr? 'link'
+    class_attr_val = type
     if (role = node.role)
-      if node.attr? 'float'
-        class_attr_val = %(#{type} #{node.attr 'float'} #{role})
-      else
-        class_attr_val = %(#{type} #{role})
-      end
+      class_attr_val = (node.attr? 'float') ? %(#{class_attr_val} #{node.attr 'float'} #{role}) : %(#{class_attr_val} #{role})
     elsif node.attr? 'float'
-      class_attr_val = %(#{type} #{node.attr 'float'})
-    else
-      class_attr_val = type
+      class_attr_val = %(#{class_attr_val} #{node.attr 'float'})
     end
     %(<span class="#{class_attr_val}">#{img}</span>)
   end
