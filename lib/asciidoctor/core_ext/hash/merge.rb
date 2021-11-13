0b073affa82bad82817b50f1cf495af1256bbb12
Dan Allen <dan.j.allen@gmail.com>
2021-11-12 23:53:40 -0700

    use dup instead of super({}) in monkeypatch for Hash#merge

--- lib/asciidoctor/core_ext/hash/merge.rb
+++ lib/asciidoctor/core_ext/hash/merge.rb
@@ -4,6 +4,6 @@
 # NOTE use `send :prepend` to be nice to Ruby 2.0
 Hash.send :prepend, (Module.new do
   def merge *args
-    (len = args.length) < 1 ? super({}) : (len > 1 ? args.inject(self) {|acc, arg| acc.merge arg } : (super args[0]))
+    (len = args.length) < 1 ? dup : (len > 1 ? args.inject(self) {|acc, arg| acc.merge arg } : (super args[0]))
   end
 end) if (Hash.instance_method :merge).arity == 1
