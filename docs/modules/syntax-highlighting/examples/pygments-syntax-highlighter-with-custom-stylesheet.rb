e0fff1f0475399174dde98da6d916e73b88a8a9f
Dan Allen <dan.j.allen@gmail.com>
2021-12-23 23:59:06 -0700

    fix syntax highlighter example to fully support pygments-stylesheet attribute

--- docs/modules/syntax-highlighting/examples/pygments-syntax-highlighter-with-custom-stylesheet.rb
+++ docs/modules/syntax-highlighting/examples/pygments-syntax-highlighter-with-custom-stylesheet.rb
@@ -1,12 +1,15 @@
-class MyPygmentsAdapter < (Asciidoctor::SyntaxHighlighter.for 'pygments')
+class ExtendedPygmentsSyntaxHighlighter < (Asciidoctor::SyntaxHighlighter.for 'pygments')
   register_for :pygments
 
-  def write_stylesheet? doc
-    false
-  end
-
   def docinfo location, doc, opts
-    slash = opts[:self_closing_tag_slash]
-    %(<link rel="stylesheet" href="/styles/syntax-theme.css"#{slash}>)
+    stylesheet = doc.attr 'pygments-stylesheet', './pygments.css'
+    if opts[:linkcss]
+      %(<link rel="stylesheet" href="#{stylesheet}"#{opts[:self_closing_tag_slash]}>)
+    else
+      stylesheet = doc.normalize_system_path stylesheet
+      %(<style>
+#{doc.read_asset stylesheet, label: 'stylesheet', normalize: true}
+</style>)
+    end
   end
 end
