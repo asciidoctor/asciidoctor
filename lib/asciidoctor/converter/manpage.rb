35a4aeb489d8f25022bf32fd6e4d00d5b6347c99
Dan Allen <dan.j.allen@gmail.com>
2023-05-25 18:38:25 -0600

    use single-line block in manify method in manpage converter to make it easier to patch

--- lib/asciidoctor/converter/manpage.rb
+++ lib/asciidoctor/converter/manpage.rb
@@ -726,9 +726,7 @@ allbox tab(:);'
       .gsub(LiteralBackslashRx) { $1 ? $& : '\\(rs' } # literal backslash (not a troff escape sequence)
       .gsub(EllipsisCharRefRx, '...') # horizontal ellipsis
       .gsub(LeadingPeriodRx, '\\\&.') # leading . is used in troff for macro call or other formatting; replace with \&.
-      .gsub EscapedMacroRx do # drop orphaned \c escape lines, unescape troff macro, quote adjacent character, isolate macro line
-        (rest = $3.lstrip).empty? ? %(.#{$1}"#{$2}") : %(.#{$1}"#{$2.rstrip}"#{LF}#{rest})
-      end
+      .gsub(EscapedMacroRx) { (rest = $3.lstrip).empty? ? %(.#{$1}"#{$2}") : %(.#{$1}"#{$2.rstrip}"#{LF}#{rest}) } # drop orphaned \c escape lines, unescape troff macro, quote adjacent character, isolate macro line
       .gsub('-', '\-')
       .gsub('&lt;', '<')
       .gsub('&gt;', '>')
