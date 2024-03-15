564d8dc5b1faf1964ec49d1d4156c2b26ec3899c
Dan Allen <dan.j.allen@gmail.com>
2024-03-15 02:20:03 -0600

    fix typo in test description

--- test/lists_test.rb
+++ test/lists_test.rb
@@ -379,7 +379,7 @@ context "Bulleted lists (:ulist)" do
       assert_xpath '//ul/li/*[@class="admonitionblock note"]//td[@class="content"][normalize-space(text())="This is a note."]', output, 1
     end
 
-    test 'paragraph-like blocks attached to an ancestry list item by a list continuation should produce blocks' do
+    test 'paragraph-like blocks attached to an ancestor list item by a list continuation should produce blocks' do
       # NOTE cannot use single-quoted heredoc because of https://github.com/jruby/jruby/issues/4260
       input = <<~EOS
       * parent
