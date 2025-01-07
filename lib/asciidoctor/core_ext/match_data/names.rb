bdd1f841c7a396c6e10a1d7599e3a5cf40de5d51
Adam Hess <hparker@github.com>
2025-01-07 11:38:49 -0800

    remove core extensions for older rubies and opal
    Since Ruby 2.7 is the minimum supported Ruby, these core extensions can be removed.
    
    Also Opal has implemented regex capture names, so we can also remove that patch

--- lib/asciidoctor/core_ext/match_data/names.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# NOTE remove once implemented in Opal; see https://github.com/opal/opal/issues/1964
-class MatchData
-  def names
-    []
-  end
-end unless MatchData.method_defined? :names
